85bc670824288a4a0abee0bd2ead49049b8e7ffd
Phat Nguyen <masterrio2412@gmail.com>
2026-05-13 16:42:15 +0700

    fix api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
-const API_BASE_URL = `${baseUrl.replace(/\/$/, "")}/api`;
+const API_BASE_URL = `${baseUrl.replace(/\/$/, "")}`;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
